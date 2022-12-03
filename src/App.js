d0148d8ac42bf40796d803a5b87301edd11b60f7
W-ill-c <williamcollett36@gmail.com>
2022-12-03 18:25:41 +0000

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" />
+      <Route path="/services"/>
+    </Routes>
+  );
+}
+
+export default App;
